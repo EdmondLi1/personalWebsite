1fad18307dfdfa25e9bfa30a702de1738612c905
Edmond <liedmond0527@gmail.com>
2024-01-01 21:49:07 -0500

    added the home page

---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+import HomeData from './HomeData';
+import './Home.css';
+import { IconMouse } from '@tabler/icons-react';
+
+// home socials
+const homeSocials = [
+    {
+      icon: <Icon icon="simple-icons:devpost" className="text-xl md:mb-1.5 text-title-color 
+      hover:text-title-color-dark hover:scale-105 transition-all" />,
+      link: 'https://devpost.com/liedmond0527',
+    },
+    {
+      icon: <i className="fa-brands fa-github text-lg ml-[2px] text-title-color 
+          hover:text-title-color-dark hover:scale-105 transition-all"></i>,
+      link: 'https://github.com/EdmondLi1',
+    },
+    {
+      icon: <i className="fa-brands fa-linkedin text-lg ml-[3px] text-title-color 
+          hover:text-title-color-dark hover:scale-105 transition-all"></i>,
+      link: 'https://www.linkedin.com/in/edmond-li2004/',
+    } 
+];
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content max-md:flex max-md:flex-col md:flex items-center justify-center pt-22 gap-x-8">
+            <div className="home__social grid gap-y-1 items-center max-md:flex max-md:flex-row max-md:justify-center">
+              {homeSocials.map((item, index) => (
+                    <a href={item.link} className="socials__link" key={index}>
+                        {item.icon}
+                    </a>
+                  ))
+                }
+            </div>
+            <img src="./EDMOND_PROFILE.jpg" alt="Edmond Li" className="home__img shadow-2xl rounded-full"/>
+            <div className="home__data">
+                <HomeData />
+            </div>
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home;
---
+++ client/src/components/Home/HomeData.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button } from 'antd';
+import { IconSend } from '@tabler/icons-react';
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__greeting text-title-color font-bold text-5xl max-md:text-4xl mb-3"> 
+        Welcome! :D 
+      </h1>
+      <h1 className="home__title text-title-color font-semibold text-4xl max-md:text-2xl mb-5"> 
+       Edmond Li
+      </h1>
+      <h3 className="home__subtitle text-subtitle-color font-normal text-sm mb-5 relative">
+          CS @ University of Waterloo (AI and Digital Hardware Specilization, Pure Mathematics Minor)
+      </h3>
+      <a href="#contact">
+        <Button
+          className="text-align inline-flex items-center justify-center text-sm font-semibold text-title-color hover:text-title-color-dark hover:scale-105 transition-all"
+          icon={<IconSend className="w-4"/>}
+          size="large"
+          shape="round"
+        >
+          Contact Me
+        </Button>
+      </a>
+    </div>
+  )
+}
+
+export default Data;
