1893e1c732070453e085b9abbde2ec8dd149800b
Edmond <liedmond0527@gmail.com>
2024-01-10 23:21:51 -0500

    added styling to contact section

--- client/src/components/Contact/Contact.tsx
+++ client/src/components/Contact/Contact.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
-import { Button, Form, Input } from 'antd';
+import { useState, useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Button, Form, Input, message } from 'antd';
 import './Contact.css'
 import ContactCard from './ContactCard';
 import { IconSend } from '@tabler/icons-react';
@@ -7,33 +8,23 @@ import { IconSend } from '@tabler/icons-react';
 const contactData = [
     {
         title: 'Email',
-        icon: <i className="fas fa-envelope"></i>,
+        icon: <i className="fas fa-envelope text-2xl mb-1"></i>,
         data: 'edmondli0527@gmail.com',
         link: 'mailto:edmondli0527@gmail.com'
     },
     {
         title: 'Phone',
-        icon: <i className="fas fa-phone"></i>,
+        icon: <i className="fas fa-phone text-2xl mb-1"></i>,
         data: '647-996-3527',
         link: 'tel:+16479963527'
     },
     {
         title: 'LinkedIn',
-        icon: <i className="fa-brands fa-linkedin"></i>,
+        icon: <i className="fa-brands fa-linkedin text-2xl mb-1"></i>,
         data: 'edmond-li2004',
         link: 'https://www.linkedin.com/in/edmond-li2004/'
     }
 ];
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const onFinish = (values: any) => {
-    console.log('Success:', values);
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
 
 type FormValues = {
     firstName: string;
@@ -42,10 +33,17 @@ type FormValues = {
     message: string;
 };    
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const customValidator = (message : string) => (_: any, value: any) => {
+    if (!value || value.trim() === '') {
+      return Promise.reject(new Error(message));
+    }
+    return Promise.resolve();
+};
 
 const Contact = () => {
+    const formRef = useRef(null);
     const [messageTextArea, setMessageTextArea] = useState(''); 
-    
     const [formValues, setFormValues] = useState<FormValues>({
         firstName: '',
         lastName: '',
@@ -60,15 +58,85 @@ const Contact = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+        
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const onFinish = (values: any) => {
+        console.log('form values:', values);
+
+        // if (!values.firstName || values.firstName === '' || values.firstName.trim() === '') {
+        //     message.error('Please enter a valid first name :)');
+        //     return;
+        // }
+        // if (!values.lastName || values.lastName === '' || values.lastName.trim() === '') {
+        //     message.error('Please enter a valid last name :)');
+        //     return;
+        // }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        // if (!values.email || values.email.trim() === '' || !emailRegex.test(values.email.trim())) {
+        //     message.error('Please enter a valid email :)');
+        //     return;
+        // }
+
+        if (!emailRegex.test(values.email.trim())) {
+            message.error('Please enter a valid email :)');
+            return;
+        }
+
+        // Send the email after validation
+        const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+        const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+        const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+        emailjs.send(SERVICE_ID, TEMPLATE_ID, values, PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text);
+                message.success('Email sent successfully!');
+                form.resetFields();
+            }, (error) => {
+                console.log(error.text);
+                message.error('Email failed to send :(');
+            });
+        };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    // const sendEmail = () => {
+    //     const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    //     const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    //     const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+    //     console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY);
+    //     console.log(formRef?.current)
+
+    //     if (formRef?.current) {
+    //         emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, formRef?.current, PUBLIC_KEY)
+    //             .then((result) => {
+    //                 console.log(result.text);
+    //                 message.success('Email sent successfully!');
+    //                 form.resetFields();
+    //             }, (error) => {
+    //                 console.log(error.text);
+    //                 message.error('Email failed to send :(');
+    //             });
+    //     } else {
+    //         message.error('Email failed to send :(');
+    //     }
+    // }
+
     return (
         <section className="contact section" id="contact">
             <h2 className="section__title text-2xl font-semibold">Contact Me</h2>
             <span className="section__subtitle text-sm">Info and Email</span>
 
-            <div className="contact__container container grid">
+            <div className="contact__container container grid justify-center gap-x-24 mb-12">
                 <div className="contact__content">
-                    <h3 className="contact__title"> Contact Info </h3>
-                    <div className="contact__info">
+                    <h3 className="contact__title text-center font-semibold mb-6"> Contact Info </h3>
+                    <div className="contact__info grid gap-y-4">
                         {contactData.map((contact, index) => (
                             <ContactCard
                                 key={index}
@@ -81,9 +149,10 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="contact__content">
-                    <h3 className="contact__title"> Contact Me </h3>
+                    <h3 className="contact__title text-center font-semibold mb-6"> Contact Me </h3>
                     <Form
-                        className="contact__form"
+                        ref={formRef}
+                        className="contact__form w-[360px] relative mb-8"
                         form={form}
                         layout="vertical"
                         onFinish={onFinish}
@@ -101,34 +170,40 @@ const Contact = () => {
                         <Form.Item
                             label={<p className="font-semibold">First Name</p>}
                             name="firstName"
-                            className="contact__form-item"   
+                            className="contact__form-item m-0 p-0 mb-2"
+                            rules={[{ validator: customValidator("Please enter your first name :)") }]}   
                         >
-                            <Input placeholder="John" className="contact__form-input rounded-xl"/>
+                            <Input placeholder="John" className="contact__form-input rounded-xl text-text-color"/>
                         </Form.Item>
 
                         <Form.Item
                             label={<p className="font-semibold">Last Name</p>}
                             name="lastName"
+                            className="m-0 p-0 mb-2"
+                            rules={[{ validator: customValidator("Please enter your last name :)") }]}   
                         >
-                            <Input placeholder="Smith" className="contact__form-input rounded-xl"/>
+                            <Input placeholder="Smith" className="contact__form-input rounded-xl text-text-color"/>
                         </Form.Item>
 
                         <Form.Item
                             label={<p className="font-semibold">Email</p>}
                             name="email"
+                            className="m-0 p-0 mb-2"  
+                            rules={[{ validator: customValidator("Please enter your email :D") }]}   
                         >
-                            <Input placeholder="johnsmith@gmail.com" className="contact__form-input rounded-xl"/>
+                            <Input placeholder="johnsmith@gmail.com" className="contact__form-input rounded-xl text-text-color"/>
                         </Form.Item>
 
                         <Form.Item
                             label={<p className="font-semibold">Message</p>}
-                            name="message"       
+                            name="message"   
+                            className="m-0 p-0 mb-2"    
                         >
                            <Input.TextArea 
                                 rows={2} 
                                 value={messageTextArea} 
                                 placeholder="Type your message here..."
-                                className="contact__form-input rounded-xl"
+                                className="contact__form-area rounded-xl text-text-color"
                                 onChange={(e) => {
                                     setMessageTextArea(e.target.value);
                                 }}  
@@ -140,7 +215,7 @@ const Contact = () => {
                                 htmlType="submit"
                                 size="middle"
                                 shape="round"
-                                className="font-semibold mt-4 shadow-xl inline-flex items-center px-2 py-5"
+                                className="font-semibold mt-2 shadow-xl inline-flex items-center px-2 py-5"
                                 icon={<IconSend className="w-4"/>}
                             >
                                 Send Message
--- client/src/components/Contact/ContactCard.tsx
+++ client/src/components/Contact/ContactCard.tsx
@@ -12,16 +12,16 @@ interface ContactCardProps {
 
 const ContactCard: React.FC<ContactCardProps> = ({title, icon, data, link}) => {
   return (
-    <div className="contact__card">
+    <div className="contact__card bg-container-color border rounded-xl shadow-lg p-4 text-center">
         {icon}
-        <h3 className="contact__card-title font-semibold">{title}</h3>
-        <span className="contact__card-data text-sm">{data}</span>
+        <h3 className="contact__card-title font-semibold text-title-color">{title}</h3>
+        <span className="contact__card-data block text-sm text-text-color mb-3">{data}</span>
         <Button
-            className="contact__buton inline-flex items-center justify-center font-semibold"
+            className="contact__buton inline-flex items-center justify-center font-semibold text-text-color hover:scale-105 border-title-color"
             size="middle"
             shape="round"
             onClick={() => window.location.href = link}
-            icon={<IconSend2 className="w-4"/>}
+            icon={<IconSend2 className="w-4 transition-[0.3]"/>}
         >
             Contact
         </Button>
